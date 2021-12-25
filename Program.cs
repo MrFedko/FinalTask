dd88a9f9eda7525e8bb69cdac7b920b140c4c935
MrFedko <miklerst@mail.ru>
2021-12-25 18:28:24 +0300

    array printing method

--- Program.cs
+++ Program.cs
@@ -1,13 +1,22 @@
-﻿int [] Request(string message)
+﻿int[] Request(string message)
 {
     Console.WriteLine(message);
     string line = Console.ReadLine();
     string[] num = line.Split(' ');
-    int[] array = new int [num.Length];
+    int[] array = new int[num.Length];
     for (int i = 0; i < array.Length; i++)
     {
         array[i] = Convert.ToInt32(num[i]);
     }
     return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} "); 
+    }
+    Console.WriteLine();
 }
 
