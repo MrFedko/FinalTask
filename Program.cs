5bf389ca21c63b2942f410258e7b1eceb0674d1d
MrFedko <miklerst@mail.ru>
2021-12-25 18:38:58 +0300

    only even numbers in array

--- Program.cs
+++ Program.cs
@@ -15,8 +15,23 @@ void PrintArray(int[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($"{array[i]} "); 
+        Console.Write($"{array[i]} ");
     }
     Console.WriteLine();
 }
 
+int[] EvenNumbers(int[] array)
+{
+    int[] array2 = new int[array.Length];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            array2[count] = array[i];
+            count++;
+        }
+    }
+    Array.Resize(ref array2, count);
+    return array2;
+}
