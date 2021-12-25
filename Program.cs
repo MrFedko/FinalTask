bcfda443fbbb3d1e62b020924ef7a230a2b9d479
MrFedko <miklerst@mail.ru>
2021-12-25 18:23:00 +0300

    array filling method

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int [] Request(string message)
+{
+    Console.WriteLine(message);
+    string line = Console.ReadLine();
+    string[] num = line.Split(' ');
+    int[] array = new int [num.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = Convert.ToInt32(num[i]);
+    }
+    return array;
+}
+
